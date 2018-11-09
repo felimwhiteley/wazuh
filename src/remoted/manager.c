9bc3368cbaf0ac12c547402dfa06548ec1b73287
Vikman Fernandez-Castro <victor@wazuh.com>
2018-11-09 13:26:22 +0100

    Fix agent's control message truncation in Remoted
    This bug prevents Remoted from parsing a control message that exceeds 1024 bytes.

--- src/remoted/manager.c
+++ src/remoted/manager.c
@@ -1001,12 +1001,9 @@ static void read_controlmsg(const char *agent_id, char *msg)
  */
 void *wait_for_msgs(__attribute__((unused)) void *none)
 {
-    char msg[OS_SIZE_1024 + 2];
+    char * msg;
     char agent_id[9];
     pending_data_t *data;
-
-    /* Initialize the memory */
-    memset(msg, '\0', OS_SIZE_1024 + 2);
 
     /* Should never leave this loop */
     while (1) {
@@ -1021,7 +1018,7 @@ void *wait_for_msgs(__attribute__((unused)) void *none)
         /* Pop data from queue */
         if ((data = OSHash_Get(pending_data, pending_queue[queue_j]))) {
             strncpy(agent_id, pending_queue[queue_j], 8);
-            strncpy(msg, data->message, OS_SIZE_1024);
+            os_strdup(data->message, msg);
         } else {
             merror("Couldn't get pending data from hash table for agent ID '%s'.", pending_queue[queue_j]);
             *agent_id = '\0';
@@ -1041,6 +1038,8 @@ void *wait_for_msgs(__attribute__((unused)) void *none)
         w_mutex_lock(&lastmsg_mutex);
         data->changed = 0;
         w_mutex_unlock(&lastmsg_mutex);
+
+        free(msg);
     }
 
     return (NULL);
